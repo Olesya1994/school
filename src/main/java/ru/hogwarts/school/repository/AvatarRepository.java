3bd213432566a3daba890373736952f71401ea95
Olesya1994 <olesia.liakhor@gmail.com>
2023-08-20 02:39:54 +0300

    проблема с анатацией @Query (Annotations are not allowed in repository)

--- src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
+++ src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
@@ -1,16 +1,19 @@
 package ru.hogwarts.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Faculty;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AvatarRepository extends JpaRepository<Avatar, Long> {
 
     Optional<Avatar> findByStudentId(long studentId);
+
 
 }
--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -2,6 +2,7 @@ package ru.hogwarts.school.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.model.Avatar;
@@ -16,6 +17,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
+import java.util.List;
 
 import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
@@ -73,5 +75,9 @@ public class AvatarService {
 
     public Avatar findAvatar(Long studentId) {
         return avatarRepository.findByStudentId(studentId).orElse(new Avatar());
+    }
+
+    public List<Avatar> getPage(int pageNumber, int pageSize){
+        return avatarRepository.findAll(PageRequest.of(pageNumber-1,pageSize)).getContent();
     }
 }
